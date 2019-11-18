4295cb6506f7badfad4937c1c513ae188ee8aae7
Surekha <surekhas.ttl@tatamotors.com>
2019-11-18 12:25:57 +0530

    first commit

---
+++ TravisSDemo/TravisSDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravisSDemo
+//
+//  Created by Apple on 18/11/19.
+//  Copyright © 2019 Tata Motors. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
